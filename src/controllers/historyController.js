f43eab23fed59c158836348586b5041823ca0e53
william-keller <kellerwilliam2011@gmail.com>
2020-09-22 23:34:54 -0300

    Update historyController.js

--- src/controllers/historyController.js
+++ src/controllers/historyController.js
@@ -8,11 +8,8 @@ const History = require('../models/historyModel');
 const getHistory = express.Router();
 getHistory.get('/', async(req, res) => {
     try {
-
         const histories = await History.find({});
-        
         return res.status(200).send(histories);
-
     } catch(exception) {
         return res.status(400).send({
             error : 'Não foi possivel retornar o histórico',
@@ -24,11 +21,8 @@ getHistory.get('/', async(req, res) => {
 // ROTA PARA RETORNAR TODAS AS COMPRAS REALIZADAS POR UM DETERMINADO CLIENTE
 getHistory.get('/:clientId', async(req, res) => {
     try {
-
         const histories = await History.find({client_id: req.params.clientId});
-
         return res.status(200).send(histories);
-
     } catch(exception) {
         return res.status(400).send({
             error : 'Não foi possivel retornar o historico',
